7b3a79ee19b06dce6bd502b367c0bb7b7bf818fc
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-03-14 15:06:11 -0400

    add form to table for btter structure and modify js to collect all data

--- scripts/intro_form.js
+++ scripts/intro_form.js
@@ -5,19 +5,26 @@ function getData()
   var personalBackground = "Personal Background: " + document.getElementById('personal_background').value;
   var academicBackground = "Academic Background: " + document.getElementById('academic_background').value;
   var subjectBackground = "Subject Background: " + document.getElementById('subject_background').value;
-  var courses = "Current Courses: " + document.getElementById('courses').value;
   var interestingFact = "Interesting Fact: " + document.getElementById('interesting_fact').value; 
+  let formInfo = [fullName, computerPlatform, personalBackground, academicBackground, subjectBackground, interestingFact];
+
+  var checkboxes = document.querySelectorAll('input[name="courses"]:checked');
+    var courses = [];
+    checkboxes.forEach((checkbox) => {
+        courses.push(checkbox.value);
+     });
+
+     var willGraduate = document.querySelector('input[name="graduate"]:checked').value;
+
   
-  let formInfo = [fullName, computerPlatform, personalBackground, academicBackground, subjectBackground, courses, interestingFact];
-  
-  displayData(formInfo);
+  displayData(formInfo, courses, willGraduate);
 }
 
 
-function displayData(dataArray) 
+function displayData(dataArray, courses, willGraduate) 
 {
-  
-    
+  alert(willGraduate);
+
   for (let i = 0; i < dataArray.length; i++)
   {
     //creates new element & stores in variable
@@ -36,4 +43,39 @@ function displayData(dataArray)
     position.appendChild(newListItem);
     
   }
+if (courses.length > 0){
+  var allCourses = "";
+  courses.forEach((course) => {
+    allCourses += course + ", ";
+  });
+
+  allCourses = allCourses.slice(0, -2);
+  
+    var newListItem = document.createElement('li');
+    var newText = document.createTextNode("I am enrolled in " + allCourses);
+    newListItem.appendChild(newText);
+    var position = document.getElementsByTagName('ul')[0];
+    position.appendChild(newListItem);
+} else {
+  var newListItem = document.createElement('li');
+    var newText = document.createTextNode("I am not enrolled in any courses");
+    newListItem.appendChild(newText);
+    var position = document.getElementsByTagName('ul')[0];
+    position.appendChild(newListItem);
+}
+
+var graduateText = "";
+if (willGraduate === "yes"){
+  graduateText = "I will graduate this year!"
+} else {
+  graduateText = "I will not graduate this year."
+}
+
+var newListItem = document.createElement('li');
+    var newText = document.createTextNode(graduateText);
+    newListItem.appendChild(newText);
+    var position = document.getElementsByTagName('ul')[0];
+    position.appendChild(newListItem);
+ 
+
 }
