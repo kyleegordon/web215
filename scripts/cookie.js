cad49c6c034147d8e530c1d21169552b45010c25
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-04-19 10:15:46 -0400

    more cookie

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,3 +1,32 @@
-allCookies = document.cookie;
+const currentDate = new Date();
+const timestamp = currentDate.getTime();
+const browser = window.navigator.userAgent;
 
-alert(allCookies);
+
+
+document.cookie = `browser=${browser}; timestamp=${timestamp};`;
+
+allCookies = document.cookie.split(';').reduce((cookies, cookie) => {
+    const [name, value] = cookie.split('=').map(c => c.trim());
+    cookies[name] = value;
+    return cookies;
+}, {});
+
+alert(JSON.stringify(allCookies));
+
+
+
+function showCookies() {
+    const output = document.getElementById('cookies')
+    output.textContent = '> ' + document.cookie
+}
+
+function clearOutputCookies() {
+    const output = document.getElementById('cookies')
+    output.textContent = ''
+    document.cookie = `expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+
+}
+
+
+
