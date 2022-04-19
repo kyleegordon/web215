8108b0037c93b0f5e893e0ffad81983a7d3cb20c
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-04-19 11:17:40 -0400

    set up get and post to database

--- project/server/index.js
+++ project/server/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+//convert json to objects
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://kgordon:GDdx7dEu4EBEqlTA@cluster0.30xep.mongodb.net/web215?retryWrites=true&w=majority");
+
+//get user data
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+
+    })
+});
+
+//add new user to database
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+});
 
 
 app.listen(3001, () => {
